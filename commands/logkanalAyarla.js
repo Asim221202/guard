99bb8acb6ba8d7a0dc8940d1c2fd85c243156ca9
ixaryl <rahimasim15@gmail.com>
2025-06-18 20:55:52 +0400

    Create logkanalAyarla.js

---
+++ commands/logkanalAyarla.js
@@ -0,0 +1,35 @@
+// commands/log-kanal-ayarla.js
+
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const GuardSettings = require('../models/GuardSettings');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('log-kanal-ayarla')
+    .setDescription('Guard loglarının gönderileceği kanalı ayarlar.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addChannelOption(option =>
+      option.setName('kanal')
+        .setDescription('Logların gönderileceği kanal')
+        .addChannelTypes(ChannelType.GuildText)
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const kanal = interaction.options.getChannel('kanal');
+    const guildID = interaction.guild.id;
+
+    let settings = await GuardSettings.findOne({ guildID });
+    if (!settings) {
+      settings = new GuardSettings({ guildID });
+    }
+
+    settings.logChannelID = kanal.id;
+    await settings.save();
+
+    return interaction.reply({
+      content: `✅ Guard log kanalı başarıyla ${kanal} olarak ayarlandı.`,
+      ephemeral: true
+    });
+  }
+};
